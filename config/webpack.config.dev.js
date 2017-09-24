7c389ddae205d646b3b56312c9cdfc4dd8521f9c
Hu Jiwei <hu_jiwei@foxmail.com>
2017-09-24 12:22:23 +0800

    去掉PWA相关代码，优化模板输出

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -222,6 +222,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: true,
       template: paths.appHtml,
+      isDev: true,
     }),
     // Add module names to factory functions so they appear in browser profiler.
     new webpack.NamedModulesPlugin(),
