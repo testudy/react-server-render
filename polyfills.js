76811b250e6928f526d17e5ffbda8b7d0dd4107b
Hu Jiwei <hu_jiwei@foxmail.com>
2017-09-24 13:34:14 +0800

    简单解决host问题

---
+++ polyfills.js
@@ -0,0 +1,20 @@
+// polyfills
+
+const realFetch = require('node-fetch');
+let localApp = null;
+
+function fetch(url, options) {
+    let host = localApp && localApp.locals.host || '';
+
+	if (!/^https?:\/\//.test(url)) {
+		url = 'http://' + host + url;
+	}
+	return realFetch.call(this, url, options);
+};
+
+module.exports = function(app) {
+    localApp = app;
+    if (!global.fetch) {
+	    global.fetch = fetch;
+    }
+};
